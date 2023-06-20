812587c9c309fbf69a655deea09c41490c4a1e49
lipatov-fl <lipatov-fl@yandex.ru>
2023-06-20 22:17:51 +0400

    finished mvvm job

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -3,7 +3,7 @@ package ru.netology.nmedia.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.Post
-import ru.netology.nmedia.R
+
 
 class PostRepositoryInMemoryImpl: PostRepository {
     private var post = Post(
